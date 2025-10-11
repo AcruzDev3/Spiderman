d8d0293ffd8378048e51920b8ac5b417eae90c5e
AcruzDev3 <acruzdev03@gmail.com>
2025-10-11 21:34:17 +0200

    Implementacion de interfaces para las clases viewModels y managers

---
+++ LIB/Interfaces/IManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB.Interfaces
+{
+    internal interface IManager<T>
+    {
+        Task<T> GetOne(int id);
+
+        Task<List<T>> GetAll();
+
+        Task<int> Create(T viewModel);
+
+        Task<int> Delete(int id);
+
+        Task<int> Exists(T viewModel);
+    }
+}
---
+++ LIB/Interfaces/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB.Interfaces
+{
+    public interface IViewModel<T>
+    {
+       void Create(T model);
+    }
+}
