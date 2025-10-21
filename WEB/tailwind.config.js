21212e96462e870e81db2d39c976fdb5a8b22f98
AcruzDev3 <acruzdev03@gmail.com>
2025-10-21 19:45:21 +0200

    Estilo del header

--- WEB/tailwind.config.js
+++ WEB/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
     content: ['./src/**/*.{html,ts}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                "venom-black": '#0b0b0b',
+                "spider-red": '#e10600',
+                "neon-purple": '#6c2dc7',
+                "deep-violet": '#2e003e',
+                "eletric-blue": '#00cfff',
+                "electric-yellow": '#fffb00',
+                "smoke-gray": '#b0b0b0',
+                "white-smoke": '#f5f5f5'
+            }
+        }
     },
     plugins: []
 }
